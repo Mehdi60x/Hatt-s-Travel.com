5f1975410465f0e4dbd0b7fa704d4f0e5e4bc0a4
mehdi.hattaoui@hotmail.com <mehdi.hattaoui@hotmail.com>
2024-10-24 23:12:30 +0200

    miseajour

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
+
 var toggle_menu = document.querySelector('.responsive-menu');
 var menu = document.querySelector('.menu');
 toggle_menu.onclick= function(){
     toggle_menu.classList.toggle('active');
     menu.classList.toggle('responsive')
-        alert("vous avez cliqué sur le menu")
+ 
 }
+
+
